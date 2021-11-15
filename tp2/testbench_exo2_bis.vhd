0fa044b516870b6310a9b01c0e7f7e6dd4b20145
adriengui <55453387+adriengui@users.noreply.github.com>
2021-11-15 18:44:19 +0100

    Add files via upload

---
+++ tp2/testbench_exo2_bis.vhd
@@ -0,0 +1,22 @@
+entity test is
+end test;
+
+architecture bench of test is
+
+component compteur is -- Comme toujours, on recopie ici l'entité du composant
+port(clk, rst, E, S:bit;
+	cup:out bit);
+end component;
+
+signal clk, rst, E, S, cup:bit; -- On définit les signaux d'entrée/sortie
+
+begin
+	UUT:compteur port map(clk=>clk, rst=>rst, E=>E, S=>S, cup=>cup);
+	-- Dans le port map on fait l'assignation par nom de tous les signaux	
+
+	clk<=not clk after 10 ns; -- On décrit un scénario permettant de 
+	-- tester à peu près toutes les possibilités du système
+	rst<='1';
+	E<='1' after 28 ns, '0' after 68 ns, '1' after 108 ns;
+	S<='1' after 148 ns;
+end bench;
