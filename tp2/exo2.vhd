3bc5da51bb459fbb2cfc7b6c8c949f4334ee6f6e
adriengui <55453387+adriengui@users.noreply.github.com>
2021-11-15 18:20:11 +0100

    Add files via upload

--- tp2/exo2.vhd
+++ tp2/exo2.vhd
@@ -1,40 +1,43 @@
 entity compteur is
-port(clk, rst, E, S:bit;
+port(clk, rst, E, S:bit; -- L'entité du composant est très simple
 	cup:out bit);
 end compteur;
 
 architecture biprocess of compteur is
 
-type defetat is(q0, q1, q2);
+type defetat is(q0, q1, q2); -- Comme on utilise deux process, on définit toujours état et nétat comme des signaux
 signal etat,netat:defetat;
 
 begin
-	mem:process(rst, clk)
+	mem:process(rst, clk) -- Le process de mémorisation est toujours le meme
 	begin
 		if rst='0' then
-			etat<=q0;
+			etat<=q0; -- Sur le rst on revient à l'état initial Q0
 		elsif clk='1' and clk'event then
 			etat<=netat;
 		end if;
 	end process;
 
-	comb:process(etat, E, S)
+	comb:process(etat, E, S) -- Ici en plus de l'état, on met les signaux d'entrée E et S
 	begin
-		cup<='0';
+		cup<='0'; -- On réinitialise les sorties et on met à jour le nétat
 		netat<=etat;
 
-		case etat is
+		case etat is -- Puis enfin on décrit le fonctionnement de la machine avec un case
 			when q0 => 
 				if E='1' then 
 					if S='1' then 
 						netat<=q2; 
-						cup<='1'; 
+						cup<='1'; -- S'agissant d'une machine de Mealy, les sorties dépendant de l'état interne
+							-- et des entrées, contrairement au controleur précédent.
+						-- Dans Q0, on allume la sortie lors du décomptage car on passe à Q2
 					else 
 						netat<=q1; 
 					end if; 
 				end if;
 			when q1 => 
-				if E='1' then 
+				if E='1' then -- A chaque fois, on teste donc en premier E, puis S car si enable est désactivé,
+						-- il ne faut rien faire
 					if S='1' then 
 						netat<=q0; 
 					else 
@@ -47,7 +50,7 @@ begin
 						netat<=q1; 
 					else 
 						netat<=q0; 
-						cup<='1'; 
+						cup<='1'; -- Dans Q2, allume la sortie lors du comptage car on passe à Q0
 					end if;
 				end if;
 		end case;
--- tp2/testbench_exo2.vhd
+++ tp2/testbench_exo2.vhd
@@ -3,17 +3,20 @@ end test;
 
 architecture bench of test is
 
-component compteur is
+component compteur is -- Comme toujours, on recopie ici l'entité du composant
 port(clk, rst, E, S:bit;
 	cup:out bit);
 end component;
 
-signal clk, rst, E, S, cup:bit;
+signal clk, rst, E, S, cup:bit; -- On définit les signaux d'entrée/sortie
 
 begin
 	UUT:compteur port map(clk=>clk, rst=>rst, E=>E, S=>S, cup=>cup);
-	clk<=not clk after 10 ns;
-	rst<='1';--, '0' after 85 ns, '1' after 115 ns;--, '0' after 200 ns, '1' after 250 ns;
-	E<='1' after 100 ns;--, '0' after 140 ns, '1' after 170 ns;
-	S<='1' after 225 ns;
+	-- Dans le port map on fait l'assignation par nom de tous les signaux	
+
+	clk<=not clk after 10 ns; -- On décrit un scénario permettant de 
+	-- tester à peu près toutes les possibilités du système
+	rst<='1', '0' after 235 ns, '1' after 245 ns;
+	E<='1' after 35 ns;
+	S<='1' after 155 ns;
 end bench;
