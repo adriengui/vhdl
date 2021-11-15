3549e1057cc41200c9d88c08cc207a4a477c6bf4
adriengui <55453387+adriengui@users.noreply.github.com>
2021-11-15 16:53:39 +0100

    Update testbench_exo1.vhd

--- tp2/testbench_exo1.vhd
+++ tp2/testbench_exo1.vhd
@@ -13,7 +13,7 @@ signal H, rst, ready, readwrite, oe, we:bit;
 for UUT:controleur use entity work.controleur(monoprocess);
 
 begin
-	UUT:controleur port map(H=>H, rsst=>rst, ready=>ready, readwrite=>readwrite, oe=>oe, we=>we);
+	UUT:controleur port map(H=>H, rst=>rst, ready=>ready, readwrite=>readwrite, oe=>oe, we=>we);
 	H<=not H after 10 ns;
 	rst<='1', '0' after 85 ns, '1' after 115 ns;--, '0' after 200 ns, '1' after 250 ns;
 	ready<='1' after 25 ns, '0' after 55 ns, '1' after 125 ns, '0' after 135 ns, '1' after 150 ns, '0' after 168 ns, '1' after 195 ns, '0' after 300 ns, '1' after 400 ns;
