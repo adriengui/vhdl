db9b176fba0778b2d7709cd1bfd8ea6808995729
adriengui <55453387+adriengui@users.noreply.github.com>
2021-11-15 16:30:57 +0100

    Add files via upload

---
+++ tp2/exo2.vhd
@@ -0,0 +1,36 @@
+entity compteur is
+port(clk, rst, E, S:bit;
+	cup:out bit);
+end compteur;
+
+architecture biprocess of compteur is
+type defetat is(q0, q1, q2);
+signal etat,netat:defetat;
+begin
+	mem:process(rst, clk)
+	begin
+		if rst='0' then
+			etat<=q0;
+		elsif clk='1' and clk'event then
+			etat<=netat;
+		end if;
+	end process;
+
+	comb:process(etat, E, S)
+	begin
+		cup<='0';
+
+		netat<=etat;
+		case etat is
+			when q0 => if E='1' then 
+				if S='1' then netat<=q2; cup<='1'; else netat<=q1; end if; 
+			end if;
+			when q1 => if E='1' then 
+				if S='1' then netat<=q0; else netat<=q2; end if; 
+			end if;
+			when q2 => if E='1' then
+				if S='1' then netat<=q1; else netat<=q0; cup<='1'; end if;
+			end if;
+		end case;
+	end process;
+end biprocess;
---
+++ tp2/testbench_exo2.vhd
@@ -0,0 +1,18 @@
+entity test is
+end test;
+
+architecture bench of test is
+component compteur is
+port(clk, rst, E, S:bit;
+	cup:out bit);
+end component;
+
+signal clk, rst, E, S, cup:bit;
+
+begin
+UUT:compteur port map(clk, rst, E, S, cup);
+clk<=not clk after 10 ns;
+rst<='1';--, '0' after 85 ns, '1' after 115 ns;--, '0' after 200 ns, '1' after 250 ns;
+E<='1' after 100 ns;--, '0' after 140 ns, '1' after 170 ns;
+S<='1' after 225 ns;
+end bench;
