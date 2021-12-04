7927afc23048cb4c7a5e63f219d4ac136b4933b2
adriengui <55453387+adriengui@users.noreply.github.com>
2021-12-04 18:59:33 +0100

    Add files via upload

---
+++ ds/exo1.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity diviseur is
+port(h,rst:bit;
+	v:natural range 0 to 3;
+	hd:out bit);
+end diviseur;
+
+architecture dflow of diviseur is
+signal i:unsigned(3 downto 0):="0000";
+begin
+hd<=to_bit(i(v));
+i<=(i+1) mod 16 when h'event and h='1' else "0000" when rst='1';
+end dflow;
+
